f5e1e1f1565fa3903370a4abc09dfcdceaa6004b
Pasindu_SD <dilharawijesinghe86@gmail.com>
2026-01-04 00:47:25 +0530

    summery

--- train.py
+++ train.py
@@ -1,24 +1,17 @@
-# 1️⃣ Import required tools
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from features import extract_features
 import joblib
 
-# 2️⃣ Load training data
 data = pd.read_csv("urls.csv")
-data.head()
 
-# 3️⃣ Prepare data for AI
-X = data["url"].apply(extract_features).tolist()  # URL → numbers
-y = data["label"]                                # label = 0 or 1
+X = data["url"].apply(extract_features).tolist()
+y = data["label"]
 
-# 4️⃣ Create AI model
-model = RandomForestClassifier()  # Random Forest = easy & strong
+model = RandomForestClassifier()
 
-# 5️⃣ Teach AI
 model.fit(X, y)
 
-# 6️⃣ Save AI brain
 joblib.dump(model, "model.pkl")
 
 print("AI is trained and saved!")
