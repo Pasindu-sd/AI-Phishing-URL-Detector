0a38cda8ce8681298ae6cc43b1cc808450525fca
Pasindu_SD <dilharawijesinghe86@gmail.com>
2026-01-04 01:23:15 +0530

    Summery update
    finish basic AI recognitions

--- train.py
+++ train.py
@@ -6,12 +6,15 @@ import joblib
 data = pd.read_csv("urls.csv")
 
 X = data["url"].apply(extract_features).tolist()
-y = data["label"]
+y = data["label"].str.strip().str.lower().map({"legitimate": 0,"phishing": 1})
+
+# Debug check
+print("Number of NaN labels:", y.isna().sum())
 
 model = RandomForestClassifier()
 
+print(data[y.isna()])
 model.fit(X, y)
 
 joblib.dump(model, "model.pkl")
-
 print("AI is trained and saved!")
